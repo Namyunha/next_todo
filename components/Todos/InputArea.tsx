57504afccb4d8d1a9adce8bc72dbcd463914c3eb
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-05-06 20:54:10 +0900

    Add: next ui: popover

--- components/Todos/InputArea.tsx
+++ components/Todos/InputArea.tsx
@@ -1,8 +1,8 @@
 "use client";
-
 import { Input } from "@nextui-org/input";
 import { useState } from "react";
 import { Button } from "@nextui-org/button";
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
 
 export default function InputArea() {
   const [newTodoInputValue, setNewTodoInputValue] = useState("");
@@ -10,12 +10,24 @@ export default function InputArea() {
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Input onValueChange={setNewTodoInputValue} label="새로운 할 일" />
-      <Button
-        className="h-14"
-        color={newTodoInputValue.length > 0 ? "warning" : "default"}
-      >
-        추가
-      </Button>
+      <Popover placement="top" offset={20} showArrow>
+        <PopoverTrigger>
+          <Button
+            className="h-14"
+            color={newTodoInputValue.length > 0 ? "warning" : "default"}
+          >
+            추가
+          </Button>
+        </PopoverTrigger>
+        {newTodoInputValue.length === 0 && (
+          <PopoverContent>
+            <div className="px-1 py-2">
+              <div className="text-small font-bold">👊</div>
+              <div className="text-tiny">할 일을 입력해주세요</div>
+            </div>
+          </PopoverContent>
+        )}
+      </Popover>
     </div>
   );
 }
