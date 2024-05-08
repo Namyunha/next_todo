5e1e7eaa8512ba36a68a282e62477d07de7bb558
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-05-08 16:30:26 +0900

    add optioon ui

--- components/Todos/todos-table.tsx
+++ components/Todos/todos-table.tsx
@@ -8,8 +8,14 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  Dropdown,
+  DropdownTrigger,
+  Button,
+  DropdownItem,
+  DropdownMenu,
 } from "@nextui-org/react";
 import { Todo } from "@/types";
+import { VerticalDotsIcon } from "./VerticalDotsIcon";
 
 function TodosRow({ todo }: { todo: Todo }) {
   return (
@@ -21,6 +27,22 @@ function TodosRow({ todo }: { todo: Todo }) {
       </TableCell>
       <TableCell className="text-center">
         {todo.created_at.toString()}
+      </TableCell>
+      <TableCell className="text-center">
+        <div className="relative flex justify-end items-center gap-2">
+          <Dropdown className="bg-background border-1 border-default-200">
+            <DropdownTrigger>
+              <Button isIconOnly radius="full" size="sm" variant="light">
+                <VerticalDotsIcon className="text-default-400" />
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu>
+              <DropdownItem>View</DropdownItem>
+              <DropdownItem>Edit</DropdownItem>
+              <DropdownItem>Delete</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
       </TableCell>
     </TableRow>
   );
@@ -34,6 +56,7 @@ const TodosTable = ({ todos }: { todos: Todo[] }) => {
         <TableColumn className="text-center">할 일</TableColumn>
         <TableColumn className="text-center">상 태</TableColumn>
         <TableColumn className="text-center">날 짜</TableColumn>
+        <TableColumn className="text-center">설 정</TableColumn>
       </TableHeader>
       <TableBody emptyContent={"보여줄 데이터가 없습니다."}>
         {todos && todos.map((todo: Todo) => TodosRow({ todo }))}
